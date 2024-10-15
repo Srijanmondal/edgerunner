cfb169a10c414d37b70a6c0f69b06df76e937e42
Srijan Mondal <90636706+Srijanmondal@users.noreply.github.com>
2024-10-15 18:31:34 +0530

    Update script.js

--- script.js
+++ script.js
@@ -29,8 +29,8 @@ const playpauseicon = document.querySelector("#play-pause-icon")
 
 //Songs 
 const songs = [
-  {ele: Sunflower, audioName: "Sunflower", artist: "Post Malone", img: "src/img/music/Sunflower.jpg"},
   {ele: Themachine, audioName: "The Machine", artist: "Aurora Olivas & Reed Wonder ", img: "src/img/music/themachine.jpg"},
+  {ele: Sunflower, audioName: "Sunflower", artist: "Post Malone", img: "src/img/music/Sunflower.jpg"},
   {ele: Chemtrails, audioName: "Chemtrails", artist: "Lana Del Rey", img: "src/img/music/chemtrails.jpg"},
   {ele: Lustforlife, audioName: "Lust for life", artist: "Lana Del Rey & The Weeknd", img: "src/img/music/lust.jpg"},
   {ele: Thenightwemet, audioName: "The night we met", artist: "Lord Huron", img: "src/img/music/nightwemet.jpg"},
