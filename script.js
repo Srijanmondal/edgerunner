c5e513b81f1ae65d47e8af8fc543a171196f8595
Srijanmondal <srijanmondal49@gmail.com>
2025-10-26 20:48:01 +0530

    Revert "Added preload function to the music player and a smooth swapping animation"
    This reverts commit 6d7f4527026ea3345d7e12a5f0d31524b9c49d99.

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
+// preload
+//const imageUrls = ["src/img/music/Perfect.jpg",
+               //   "src/img/music/Sunflower.jpg",
+               //   "src/img/music/chemtrails.jpg",
+               //   "src/img/music/dawnfm.jpg",
+               //   "src/img/music/hurryuptomorrow.jpg",
+              //    "src/img/music/itsnotsobad.jpg",
+              //    "src/img/music/loveonme.jpg",
+              //    "src/img/music/lust.jpg",
+              //    "src/img/music/nightwemet.jpg",
+              //    "src/img/music/save your tears.jpg",
+              //    "src/img/music/smtjstlktis.png",
+               //   "src/img/music/softcore.jpg",
+               //   "src/img/music/themachine.jpg",
+               //   "src/img/music/unwritten.jpg",];
+// const preloadedImages = preloadImages(imageUrls);
+
+
 // Files 
 const Sunflower = new Audio("src/mp3/Sunflower.mp3") 
 const Lustforlife = new Audio("src/mp3/Lust for life.mp3")
@@ -6,13 +24,13 @@ const Somethingjustlikethis = new Audio("src/mp3/Something just like this.mp3")
 const Saveyourtears = new Audio("src/mp3/Save your Tears.mp3")
 const Chemtrails = new Audio("src/mp3/Chemtrails.mp3")
 const Itsnotsobad = new Audio("src/mp3/itsnotsobad.mp3")
-const Someoneelse = new Audio("src/mp3/someoneelse.mp3")
+const Someoneelse= new Audio("src/mp3/someoneelse.mp3")
 const Loveonme = new Audio("src/mp3/loveonme.mp3")
-const Thenightwemet = new Audio("src/mp3/nightwemet.mp3")
-const Softcore = new Audio("src/mp3/softcore.mp3")
-const Unwritten = new Audio("src/mp3/unwritten.mp3")
-const Themachine = new Audio("src/mp3/themachine.mp3")
-const Baptized = new Audio("src/mp3/Baptized.mp3")
+const Thenightwemet= new Audio("src/mp3/nightwemet.mp3")
+const Softcore= new Audio("src/mp3/softcore.mp3")
+const Unwritten= new Audio("src/mp3/unwritten.mp3")
+const Themachine= new Audio("src/mp3/themachine.mp3")
+const Baptized= new Audio("src/mp3/Baptized.mp3")
 
 // Elements 
 const prevbtn = document.querySelector(".previous")
@@ -23,36 +41,29 @@ const songtitle = document.querySelector(".song-title")
 const artist = document.querySelector(".artist")
 const playpauseicon = document.querySelector("#play-pause-icon")
 
-// Songs 
+//Songs 
 const songs = [
-  { ele: Sunflower, audioName: "Sunflower", artist: "Post Malone", img: "src/img/music/Sunflower.jpg" },
-  { ele: Baptized, audioName: "Baptized in fear", artist: "The Weeknd", img: "src/img/music/hurryuptomorrow.jpg" },
-  { ele: Themachine, audioName: "Themachine", artist: "Reed Wonder & Aurora Olivas", img: "src/img/music/themachine.jpg" },
-  { ele: Chemtrails, audioName: "Chemtrails", artist: "Lana Del Rey", img: "src/img/music/chemtrails.jpg" },
-  { ele: Lustforlife, audioName: "Lust for life", artist: "Lana Del Rey & The Weeknd", img: "src/img/music/lust.jpg" },
-  { ele: Thenightwemet, audioName: "The night we met", artist: "Lord Huron", img: "src/img/music/nightwemet.jpg" },
-  { ele: Softcore, audioName: "Softcore", artist: "Neighbourhood", img: "src/img/music/softcore.jpg" },
-  { ele: Someoneelse, audioName: "Someone else", artist: "The Weeknd", img: "src/img/music/dawnfm.jpg" },
-  { ele: Unwritten, audioName: "Unwritten", artist: "Natasha Bedingfield", img: "src/img/music/unwritten.jpg" },
-  { ele: Loveonme, audioName: "Love on me", artist: "Jtbazz", img: "src/img/music/loveonme.jpg" },
-  { ele: Perfect, audioName: "Perfect", artist: "Ed Sheeran", img: "src/img/music/Perfect.jpg" },
-  { ele: Itsnotsobad, audioName: "It's not so bad", artist: "Ilkay Sencan and Yves V", img: "src/img/music/itsnotsobad.jpg" },
-  { ele: Somethingjustlikethis, audioName: "Something like this", artist: "Cold play", img: "src/img/music/smtjstlktis.png" },
-  { ele: Saveyourtears, audioName: "Save your tears", artist: "The Weeknd", img: "src/img/music/save your tears.jpg" },
+  {ele: Sunflower, audioName: "Sunflower", artist: "Post Malone", img: "src/img/music/Sunflower.jpg"},
+  {ele: Baptized, audioName: "Baptized in fear", artist: "The Weeknd", img: "src/img/music/hurryuptomorrow.jpg"},
+  {ele: Themachine, audioName: "Themachine", artist: "Reed Wonder & Aurora Olivas", img: "src/img/music/themachine.jpg"},
+  {ele: Chemtrails, audioName: "Chemtrails", artist: "Lana Del Rey", img: "src/img/music/chemtrails.jpg"},
+  {ele: Lustforlife, audioName: "Lust for life", artist: "Lana Del Rey & The Weeknd", img: "src/img/music/lust.jpg"},
+  {ele: Thenightwemet, audioName: "The night we met", artist: "Lord Huron", img: "src/img/music/nightwemet.jpg"},
+  {ele: Softcore, audioName: "Softcore", artist: "Neighbourhood", img: "src/img/music/softcore.jpg"},
+  {ele: Someoneelse, audioName: "Someone else", artist: "The weeknd", img: "src/img/music/dawnfm.jpg"},
+  {ele: Unwritten, audioName: "Unwritten", artist: "Natasha Bedingfield", img: "src/img/music/unwritten.jpg"},
+  {ele: Loveonme, audioName: "Love on me", artist: "Jtbazz", img: "src/img/music/loveonme.jpg"},
+  {ele: Perfect, audioName: "Perfect", artist: "Ed Sheeran", img: "src/img/music/Perfect.jpg"},
+  {ele: Itsnotsobad, audioName: "It's not so bad", artist: "Ilkay Sencan and Yves V", img: "src/img/music/itsnotsobad.jpg"},
+  {ele: Somethingjustlikethis, audioName: "Something like this", artist: "Cold play", img: "src/img/music/smtjstlktis.png"},
+  {ele: Saveyourtears, audioName: "Save your tears", artist: "The Weeknd", img: "src/img/music/save your tears.jpg"},
 ];
 
-// ✅ Preload all song images (fixes lag on switching songs)
-songs.forEach(song => {
-  const img = new Image();
-  img.src = song.img;
-});
-
-// Auto next when song ends
-for (const song of songs) {
-  song.ele.addEventListener('ended', () => {
+for(const song of songs) {
+  song.ele.addEventListener('ended', ()=> {
     updateSong('next');
     playPauseSong();
-  });
+  })
 }
 
 let current = 0;
@@ -61,55 +72,49 @@ songtitle.innerHTML = songs[current].audioName;
 artist.innerHTML = songs[current].artist;
 banner.src = songs[current].img;
 
-// Smooth fade transition setup (optional but looks cool)
-banner.style.transition = "opacity 0.4s ease";
 
-playpausebtn.addEventListener('click', () => {
+playpausebtn.addEventListener('click', ()=> {
   playPauseSong();
-});
+})
 
-prevbtn.addEventListener('click', () => {
+prevbtn.addEventListener('click', ()=> {
   updateSong('prev');
   playPauseSong();
-});
+})
 
-nextbtn.addEventListener('click', () => {
+nextbtn.addEventListener('click', ()=> {
   updateSong('next');
   playPauseSong();
-});
+})
 
-const updateSong = (action) => {
+const updateSong = (action)=> {
   currentsong.pause();
   currentsong.currentTime = 0;
 
-  if (action === 'next') {
+  if(action === 'next'){
     current++;
-    if (current > songs.length - 1) current = 0;
+    if(current > songs.length -1) current = 0;
   }
-  if (action === 'prev') {
+  if(action === 'prev'){
     current--;
-    if (current < 0) current = songs.length - 1;
+    if(current < 0) current = songs.length - 1;
   }
-
   currentsong = songs[current].ele;
   songtitle.innerHTML = songs[current].audioName;
-  artist.innerHTML = songs[current].artist;
+artist.innerHTML = songs[current].artist;
+banner.src = songs[current].img;
+}
 
-  // Optional fade effect when image changes
-  banner.style.opacity = 0;
-  setTimeout(() => {
-    banner.src = songs[current].img;
-    banner.style.opacity = 1;
-  }, 200);
-};
-
-const playPauseSong = () => {
-  if (currentsong.paused) {
+const playPauseSong = ()=> {
+  if(currentsong.paused){
     currentsong.play();
     playpauseicon.className = "ph-bold ph-pause";
-  } else {
+  }
+  else{
     currentsong.pause();
     playpauseicon.className = "ph-bold ph-play";
   }
-};
+}
+
+
 
